d16583207257ab9c1a3a62cdcc902c2404a07a2f
finevine <finevine@gmail.com>
2019-08-22 23:13:39 +0200

    class creation

--- model.py
+++ model.py
@@ -1,3 +1,34 @@
 '''
 The model of the Physical Data model
 '''
+
+class Category:
+    ''' class for one of the categories to analyse '''
+    def __init__(self, cat_id, cat_name):
+        self.id = cat_id
+        self.name = cat_name
+
+
+class Product:
+    ''' A product from OpenFood Fact with only interesting attributes'''
+    def __init__(self, code, product_name, categories, nutrition_grade):
+        self.code = code
+        self.product_name = product_name
+        self.categories = categories
+        self.nutrition_grade = nutrition_grade
+
+
+class Substitute(Product):
+    ''' A class for a saved substitute '''
+    def __init__(self, code, product_name, categories, nutrition_grade, code_substitute):
+        super().__init__(code, product_name, categories, nutrition_grade)
+        # code substitute is the code of the product that it substitute
+        self.code_substitute = code_substitute
+
+
+class Favourite(Product):
+    ''' A class for saved products '''
+    def __init__(self, code, product_name, categories, nutrition_grade):
+        super().__init__(code, product_name, categories, nutrition_grade, code_favourite)
+        # code substitute is the code of the product that it substitute
+        self.code_favourite = code_favourite
---
+++ src/settings.py
@@ -0,0 +1,8 @@
+API_URL = 'https://fr-en.openfoodfacts.org/cgi/search.pl?'
+SEARCH_PARAM = {"search_terms": {},
+                "search_tag": "categories",
+                "sort_by": "unique_scans_n",
+                "page_size": 20,
+                "json": 1,}
+SEARCH_HEADER = 'user-agent": "Foodstitute - https://github.com/finevine/Projet5'
+DATABASE = 'foodstitute'
